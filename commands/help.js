f063a7a4740505e9257c30d941769d27478938e7
mauyeung <mauyeung74@gmail.com>
2017-10-07 00:23:15 -0400

    updated help messages

--- commands/help.js
+++ commands/help.js
@@ -1,14 +1,13 @@
 const Discord = require('discord.js');
 
 const run = (client, message, args) => {
-	console.log(client);
 	let embededmessage = new Discord.RichEmbed()
 	.setAuthor('The Skyebot command cheatsheet')
 	.setThumbnail(client.user.avatarURL)
 	.setDescription('All commands are prefixed with a ~')
-	.addField('Search', 'anime manga')
-	.addField('Reactions', 'lewd cry pout confused smug')
-	.addField('Music', 'play stop pause resume next')
+	.addField('Search', 'anime	manga')
+	.addField('Reactions', 'lewd	cry	pout	confused	smug')
+	.addField('Music', 'play	stop	pause	resume	next')
 	.setColor('#be92ff');
 	message.channel.send(embededmessage);
 }
