bb3685d1bef7a690761885749488de5a0b4d273c
mauyeung <mauyeung74@gmail.com>
2017-10-08 01:53:30 -0400

    moved anime and manga search functionality into utils search_core

--- commands/anime.js
+++ commands/anime.js
@@ -1,63 +1,7 @@
-const Discord = require('discord.js');
-const env = require('../env');
-const request = require('request');
+const search_core = require('../util/search_core');
 
 const run = (client, message, args) => {
-	message.channel.startTyping(2);
-	args = args.join(' ');
-	let tokenpost = {
-		url: 'https://anilist.co/api/auth/access_token',
-		formData: {grant_type:'client_credentials', client_id: env.anilist_id, client_secret: env.anilist_secret}
-	};
-	request.post(tokenpost, (error, response, result) => {
-		let token = JSON.parse(result);
-		let getshows = {
-			url: 'https://anilist.co/api/anime/search/' + args,
-			qs: {access_token: token}
-		}
-		request.get(getshows, (error, response, result) => {
-			let shows = JSON.parse(result);
-			if ('error' in shows) {
-				console.log('Error finding show');
-			} else {
-				let showarray = [];
-				let shownamearray = [];
-				let i = 0;
-				shows.forEach((show) => {
-					showarray.push(show);
-					shownamearray.push('[' + i + '] ' + show.title_english + '\n');
-					i++;
-				});
-				message.channel.send(('```css\n' + 'Please select a show number\n' + shownamearray + '\n```').replace(/,/g, "")).then((tempMessage) => {
-					message.channel.stopTyping();
-					message.channel.awaitMessages(response => !isNaN(response.content) && parseInt(response.content) < i, {
-						max: 1,
-						time: 15000,
-						errors: ['max', 'time'],
-					}).then((collected) => {
-						let showobject = showarray[parseInt(collected.first().content)];
-						let embededmessage = new Discord.RichEmbed()
-						.setAuthor(showobject.title_english, showobject.image_url_lge)
-						.setDescription(showobject.description.replace(/<\/?[^>]+(>|$)/g, ""))
-						.addField('Score', showobject.average_score, true)
-						.addField('Genres', showobject.genres, true)
-						.addField('Episodes', showobject.total_episodes, true)
-						.addField('Status', showobject.airing_status, true)
-						.setImage(showobject.image_url_lge)
-						.setColor('#be92ff');
-						collected.first().delete();
-						tempMessage.delete();
-						message.channel.send(embededmessage);
-						message.channel.stopTyping();
-					}).catch(() => {
-						console.log('Error selecting show number');
-						message.channel.stopTyping();
-						tempMessage.delete();
-					});
-				});
-			}
-		});
-	});
+	search_core.search('anime', message, args);
 }
 
 const help = {
---
+++ util/search_core.js
@@ -0,0 +1,72 @@
+const Discord = require('discord.js');
+const env = require('../env');
+const request = require('request');
+
+const search =  (media, message, args) => {
+	message.channel.startTyping(2);
+	args = args.join(' ');
+	let tokenpost = {
+		url: 'https://anilist.co/api/auth/access_token',
+		formData: {grant_type:'client_credentials', client_id: env.anilist_id, client_secret: env.anilist_secret}
+	};
+	request.post(tokenpost, (error, response, result) => {
+		let token = JSON.parse(result);
+		let get_media = {
+			url: 'https://anilist.co/api/' + media + '/search/' + args,
+			qs: {access_token: token}
+		}
+		request.get(get_media, (error, response, result) => {
+			let json_results = JSON.parse(result);
+			if ('error' in json_results) {
+				console.log('Error finding ' + args);
+			} else {
+				let media_array = [];
+				let media_name_array = [];
+				let i = 0;
+				json_results.forEach((show) => {
+					media_array.push(show);
+					media_name_array.push('[' + i + '] ' + show.title_english + '\n');
+					i++;
+				});
+				message.channel.send(('```css\n' + 'Please select the number corresponding to your search\n' + media_name_array + '\n```').replace(/,/g, "")).then((tempMessage) => {
+					message.channel.stopTyping();
+					message.channel.awaitMessages(response => !isNaN(response.content) && parseInt(response.content) < i, {
+						max: 1,
+						time: 15000,
+						errors: ['max', 'time'],
+					}).then((collected) => {
+						let selection = media_array[parseInt(collected.first().content)];
+						let embededmessage = new Discord.RichEmbed()
+						.setAuthor(selection.title_english, selection.image_url_lge)
+						.setDescription(selection.description.replace(/<\/?[^>]+(>|$)/g, ""))
+						.addField('Score', selection.average_score, true)
+						.addField('Genres', selection.genres, true)
+						.setImage(selection.image_url_lge)
+						.setColor('#be92ff');
+						if (media === 'anime') {
+							embededmessage
+							.addField('Episodes', selection.total_episodes, true)
+							.addField('Status', selection.airing_status, true);
+						} else {
+							embededmessage
+							.addField('Chapters', selection.total_chapters, true)
+							.addField('Status', selection.publishing_status, true)
+						}
+						collected.first().delete();
+						tempMessage.delete();
+						message.channel.send(embededmessage);
+						message.channel.stopTyping();
+					}).catch(() => {
+						console.log('Error selecting search number');
+						message.channel.stopTyping();
+						tempMessage.delete();
+					});
+				});
+			}
+		});
+	});
+}
+
+module.exports = {
+	search
+}
